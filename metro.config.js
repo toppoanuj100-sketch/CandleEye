b75d194118c84d672de6eec9f5338b8a9ca5e88c
toppoanuj100-sketch <toppoanuj100@gmail.com>
2025-12-15 00:00:50 +0530

    Add metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+// metro.config.js
+const { getDefaultConfig } = require("expo/metro-config");
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.assetExts.push("cjs");
+
+module.exports = config;
